0c20e0d0a2bb1602161069c6ca7d8ecd9c35cbcd
dhuor <dhuor2001@gmail.com>
2026-08-08 13:20:31 +0530

    modifed

--- Big_Data/DM/Lgr.py
+++ Big_Data/DM/Lgr.py
@@ -1,7 +1,13 @@
 import pandas as pd
+from sklearn.preprocessing import labelEncoder
+from sklearn.model_selection import logisticRegression
 
 # Load a local CSV file into a DataFrame
-df = pd.read_csv("data.csv")
+df = pd.read_csv("student_pass_Fail_dataset.csv")
 
 # Preview the first 5 rows
 print(df.head())
+
+#Encode Result Colum 
+#encoder = labelEncoder()
+#df["Result"] = encoder.fit
